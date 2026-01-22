1e127bd8047f15b0572ed23b22f8f93282c77d63
eracle <eracle@posteo.eu>
2026-01-22 18:12:50 +0000

    added locations

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 import Sitemap from 'vite-plugin-sitemap';
 
-import { generateDynamicSeoPaths } from './src/data/seoPaths';
+import { generateDynamicSeoPaths } from '@/data/seoPaths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
